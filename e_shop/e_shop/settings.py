6056cd5e6ee6ea02a8a82a25983a93d0d2fa3d8a
DVadim001 <dvadim001@gmail.com>
2024-02-06 19:56:54 +0500

    deploy_make_pipList

--- e_shop/e_shop/settings.py
+++ e_shop/e_shop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ji+-z)53ct7@(y*7+!3ec&f&b_!vu2v9y7wk40if@3@a3)_2v*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'e_shop.urls'
